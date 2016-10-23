f3500b333b14bf473c0a4c36edbbfde73a85cea9
Brandon Choi <bmchoi@cpp.edu>
2016-10-23 13:53:34 -0700

    Working on Board

--- Board.java
+++ Board.java
@@ -1,38 +1,40 @@
 import java.util.*;
 
 public class Board{
-  private final int boardSize = 9;
+  private final int BOARD_SIZE = 9;
   private char[] board;
+  private int emptyIndex;
 
   Board(){
+    emptyIndex = 0;
+
     //initialize board
-    board = new char[9];
-    for(int i = 0; i < board.length; i++){
-      board
+    board = new char[BOARD_SIZE];
+    for(int i = 0; i < BOARD_SIZE; i++){
+      board[i] = '0';
     }
   }
 
   public void inputBoard(String places){
     int index = 0;
-    for(int i = 0; i < 3; i++){
-      for(int j = 0; j < 3; j++){
-        board[i][j] = places.charAt(index);
-        index++;
-      }
+    for(int i = 0; i < BOARD_SIZE; i++){
+      board[i] = places.charAt(i);
+      if(places.charAt(i) == '0')
+        emptyIndex = i;
     }
   }
 
-  public Arraylist<Board> getMoves(){
+  /*public Arraylist<Board> getMoves(){
 
-  }
+  }*/
 
   public void display(){
-    for(int i = 0; i < board.length; i++){
-      for(int j = 0; j < board.length; j++){
-        System.out.print(board[i][j] + " ");
-      }
-      System.out.println();
+    for(int i = 0; i < BOARD_SIZE; i++){
+      System.out.print(i + " ");
+      if(i == 2 || i == )
+        System.out.println();
     }
+    System.out.println("empty Index:: " + emptyIndex);
   }
 
 }
